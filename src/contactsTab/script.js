cc0968ebd978088054725314f29cc45eb4174682
Lippiece <seriouss7222@gmail.com>
2022-07-31 18:03:16 +0200

    feat: add function to replace main with a contact number

--- src/contactsTab/script.js
+++ src/contactsTab/script.js
@@ -1,3 +1,12 @@
 import { css } from "@emotion/css";
 const main = document.querySelector( "main" );
 
+export default function replaceWithContacts()
+{
+	const paragraph = document.createElement( "p" ),
+		style = css( { color: "aqua" } );
+
+	paragraph.textContent = "8-800-555-35-35";
+	paragraph.classList.add( style );
+	main.replaceChildren( paragraph );
+}

d0121f67005eeb31489d27b418ca6ccb148a438a
Lippiece <seriouss7222@gmail.com>
2022-07-31 18:00:45 +0200

    feat: split initial elements into static nav and dynamic main

--- src/main/script.js
+++ src/main/script.js
@@ -6,7 +6,7 @@ const content = document.querySelector( "#content" ),
 	main = document.querySelector( "main" ),
 	mainStyle = css( { margin: "0 20vw" } );
 
-export default content;
+/* Creating an image element and appending it to a wrapper element. */
 function makeImage()
 {
 	const image = document.createElement( "img" ),
@@ -20,33 +20,55 @@ function makeImage()
 
 	return imageWrapper;
 }
-function makeButton()
+/* Creating a navbar with two buttons. */
+function makeNavbar()
 {
-	const button = document.createElement( "button" );
+	const navbar = document.createElement( "nav" ),
+		homeButton = document.createElement( "button" ),
+		menuTab = document.createElement( "button" ),
+		contactsTab = document.createElement( "button" ),
+		menuTabStyle = css( {
+			backgroundColor: "green",
+			color          : "white",
+			border         : "none",
+			padding        : "10px",
+			margin         : "10px",
+		} );
 
-	button.textContent = "Tab1";
-	button.addEventListener( "click", () =>
-	{
-		replaceWithFirstTab();
-		// import "style.css";
-	} );
+	// Home button
+	homeButton.textContent = "Home";
+	homeButton.addEventListener( "click", () =>
+	{ main.replaceChildren( makeMainContent() ) } );
+	navbar.append( homeButton );
+	// Menu tab
+	menuTab.textContent = "Menu";
+	menuTab.addEventListener( "click", () =>
+	{ replaceWithMenu() } );
+	navbar.append( menuTab );
+	// Contacts tab
+	contactsTab.textContent = "Contacts";
+	contactsTab.addEventListener( "click", () =>
+	{ replaceWithContacts() } );
+	navbar.append( contactsTab );
+	navbar.classList.add( menuTabStyle );
 
-	return button;
+	return navbar;
 }
-function makeInitialElements()
+function makeMainContent()
 {
-	// Image, headline para about the restaurant
-	const paragraph = document.createElement( "p" ),
+	// Image, headline, para about the restaurant inside a container.
+	const	container = document.createElement( "div" ),
+		paragraph = document.createElement( "p" ),
 		headline = document.createElement( "h1" ),
 		paraStyle = css( { color: "green" } );
 
-	headline.textContent  = "The Phylosophy Ingredient";
-	paragraph.textContent = "The pain itself is pain, it loves the main adipisicing process. As there is no such thing as none of our present, nor do we foresee the pain of the inventor, and for a similar reason, we are obliged to deem it. I'll explain it by flattering myself but not too harsh.";
+	headline.textContent  = "\"The Phylosophy Ingredient\" restaurant";
+	paragraph.textContent = "The pain itself is pain, it loves the container adipisicing process. As there is no such thing as none of our present, nor do we foresee the pain of the inventor, and for a similar reason, we are obliged to deem it. I'll explain it by flattering myself but not too harsh.";
 	paragraph.classList.add( paraStyle );
+	container.append( makeImage(), headline, paragraph );
 
-	return [
-		headline,
-		paragraph,
-	];
+	return container;
 }
+content.append( makeNavbar() );
+main.append( makeMainContent() );
 main.classList.add( mainStyle );
